f16b68264465ee934df7d816c64dc69a9963035c
zameermalim <63711525+zameermalim@users.noreply.github.com>
2023-09-19 21:42:51 +0530

    V1

---
+++ Kubernetes/getDeploymentsCount.py
@@ -0,0 +1,48 @@
+import subprocess
+import re
+from collections import Counter
+import yaml
+
+with open('resources/clusters.yaml', 'r') as file:
+    clusters = yaml.safe_load(file)
+
+def run_command(command):
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    output, error = process.communicate()
+    if error:
+        raise Exception(error.decode('utf-8'))
+    return output.decode('utf-8')
+
+def get_service_names(namespace):
+    # Fetch deployments from Kubernetes namespace
+    cmd_output = run_command(f"kubectl get deployment -n {namespace}")
+
+    # Split the output by lines and skip the header
+    lines = cmd_output.split('\n')[1:]
+
+    # Use regex to extract the service name
+    service_names = [re.split('-\d+', line.split()[0])[0] for line in lines if line]
+
+    # Count occurrences of each service name
+    counter = Counter(service_names)
+
+    # Sort by count
+    sorted_services = sorted(counter.items(), key=lambda x: x[1], reverse=True)
+
+    return sorted_services
+
+def main():
+
+    for cluster in clusters:
+        # Update kubeconfig for the cluster
+        run_command(cluster["config"])
+
+        # Fetch and process service names for each namespace in the cluster
+        for namespace in cluster["namespaces"]:
+            results = get_service_names(namespace)
+            print(f"\n\n Results for namespace {namespace}:")
+            for service, count in results:
+                print(f"{service}, Count: {count}")
+
+if __name__ == "__main__":
+    main()
---
+++ Kubernetes/getPodCount.py
@@ -0,0 +1,37 @@
+import subprocess
+import json
+import yaml
+
+def run_command(command):
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    output, error = process.communicate()
+    if error:
+        raise Exception(error.decode('utf-8'))
+    return output.decode('utf-8')
+
+def get_running_pods_in_namespaces():
+    # Load clusters from clusters.yaml
+    with open('resources/clusters.yaml', 'r') as file:
+        clusters = yaml.safe_load(file)
+
+    for cluster in clusters:
+        # Update kubeconfig for the current cluster
+        run_command(cluster["config"])
+
+        # Fetch all namespaces
+        namespaces_output = run_command("kubectl get namespaces -o json")
+        namespaces_json = json.loads(namespaces_output)
+
+        # Iterate over each namespace
+        for namespace_item in namespaces_json['items']:
+            namespace_name = namespace_item['metadata']['name']
+
+            # Get the number of running pods in the current namespace
+            pods_output = run_command(f"kubectl get pods -n {namespace_name} --field-selector=status.phase==Running --no-headers")
+            pod_count = len(pods_output.strip().split('\n')) if pods_output.strip() else 0
+
+            # Output the cluster, namespace, and number of running pods
+            print(f"Cluster: {cluster['config'].split('--name')[-1].strip()}, Namespace: {namespace_name}, Running Pods: {pod_count}")
+
+if __name__ == "__main__":
+    get_running_pods_in_namespaces()
