b50b95d7c66b1b54c897488303b653b0e7eda00a
zameermalim <63711525+zameermalim@users.noreply.github.com>
2023-09-22 19:13:49 +0530

    V1.2 Hitting TMS and extracting the tenant details from odata and merging. Working for DEVPROD but not NA yet

--- ElasticCluster/SegmentAndDeletedDocsStats.py
+++ ElasticCluster/SegmentAndDeletedDocsStats.py
@@ -2,89 +2,61 @@ import pandas as pd
 import requests
 from utility_functions import convert_size_to_mb, modify_volume, extract_tenant_id, convert_to_lowercase
 import os
+from config import BASE_URL, NA_API_BASE_URL, DEVPROD_API_BASE_URL, tenant_api_base_url
 
-from config import BASE_URL
-
-# Set display options for a clearer output
-
+# Set display options for clearer output
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 pd.set_option('display.max_colwidth', None)
 pd.set_option('display.max_rows', 1000)
 
 # Fetching data from the API for deleted docs stats
-indices_df = pd.DataFrame(requests.get(f'{BASE_URL}/_cat/indices/?format=json&v&s=store.size:desc').json())
-
+# indices_df = pd.DataFrame(requests.get(f'{BASE_URL}/_cat/indices/?format=json&v&s=store.size:desc').json())
 #from file
-#allocation_df = pd.read_csv("resources/NA/indicesAuditNA", sep="\s+")
+indices_df = pd.read_csv("resources/NA/indicesAuditNA", sep="\s+")
 
-# Replace None values with 0
+# Replace None values with 0 and convert columns to appropriate data types
 indices_df.fillna(0, inplace=True)
-
-# Convert the columns to the appropriate data type
 indices_df["docs.count"] = indices_df["docs.count"].astype(int)
 indices_df["docs.deleted"] = indices_df["docs.deleted"].astype(int)
-
-# Calculating the ratio of docs.deleted to docs.count
-indices_df["deleted_to_count_ratio"] = round((indices_df["docs.deleted"] / indices_df["docs.count"]) * 100 , 2)
+indices_df["deleted_to_count_ratio"] = round((indices_df["docs.deleted"] / indices_df["docs.count"]) * 100, 2)
 
 # Fetch data from the URL for segments stats
-segments_df = pd.DataFrame(requests.get(f'{BASE_URL}/_cat/segments?v&format=json').json())
-
+# segments_df = pd.DataFrame(requests.get(f'{BASE_URL}/_cat/segments?v&format=json').json())
 #From file
-#segments_df = pd.read_csv("resources/NA/segmentsAuditNA", sep="\s+")
-
+segments_df = pd.read_csv("resources/NA/segmentsAuditNA", sep="\s+")
 segment_counts = segments_df.groupby('index').size().reset_index(name='segments_count')
 
 # Load the CSV data
-csv_path = "resources/DEVPROD/telemetryDEVPROD.csv"
+csv_path = "resources/NA/telemetryNA.csv"
 telemetry_df = pd.read_csv(csv_path, sep="\s+")
-
 telemetry_df['Total'] = telemetry_df['Total'].apply(modify_volume)
-convert_to_lowercase(telemetry_df, 'TenantId')
+telemetry_df['TenantId_lowercase'] = telemetry_df['TenantId'].str.lower()
 
-# Merge the segment counts with the original segment data
+# Merge the dataframes
 merged_segments_df = pd.merge(segments_df, segment_counts, on="index", how="left")
-
-# Apply the function to extract TenantId
 merged_segments_df['TenantId_extracted'] = merged_segments_df['index'].apply(extract_tenant_id)
-
-# Now, merge using the extracted TenantId
-merged_segments_df2 = pd.merge(merged_segments_df, telemetry_df, left_on="TenantId_extracted", right_on="TenantId", how="left")
-
-# Optionally, you can drop the 'TenantId_extracted' column if not required
-merged_segments_df2.drop(columns=['TenantId_extracted'], inplace=True)
-
-# Merge the dataframes on the 'index' column
+merged_segments_df2 = pd.merge(merged_segments_df, telemetry_df, left_on="TenantId_extracted", right_on="TenantId_lowercase", how="left")
+merged_segments_df2.drop(columns=['TenantId_extracted', 'TenantId_lowercase'], inplace=True)
 final_df = pd.merge(indices_df, merged_segments_df2, on="index", how="outer")
 
-
+# Apply conversion functions and calculate statistics
 final_df['store.size'] = final_df['store.size'].apply(convert_size_to_mb)
 final_df['pri.store.size'] = final_df['pri.store.size'].apply(convert_size_to_mb)
-final_df['size'] = final_df['size'].apply(convert_size_to_mb)
-
-# Convert 'size' column to numeric
-final_df['size'] = pd.to_numeric(final_df['size'], errors='coerce')
-
-
-# Calculate the required statistics for the 'size' column for each index
-final_df['mean_size'] = final_df.groupby('index')['size'].transform('mean')
-final_df['variance_size'] = final_df.groupby('index')['size'].transform('var')
-final_df['95th_percentile_size'] = final_df.groupby('index')['size'].transform(lambda x: x.quantile(0.95))
-final_df['std_dev_size(mb)'] = final_df.groupby('index')['size'].transform('std')
-
-
-# Print the result
-#print(final_df)
-
-# Drop duplicates based on the 'index' column and keep the first occurrence
+final_df['size'] = pd.to_numeric(final_df['size'].apply(convert_size_to_mb), errors='coerce')
+final_df['mean_size'] = final_df.groupby('index')['size'].transform('mean').round(2)
+final_df['95th_percentile_size'] = final_df.groupby('index')['size'].transform(lambda x: x.quantile(0.95)).round(2)
+final_df['std_dev_size(mb)'] = final_df.groupby('index')['size'].transform('std').round(2)
 unique_df = final_df.drop_duplicates(subset='index', keep='first')
 
+# Fetch all tenants
+response = requests.get(tenant_api_base_url, headers={'accept': 'application/json'})
+if response.status_code == 200:
+    all_tenants_data = response.json().get('value', [])
+    all_tenants_df = pd.DataFrame(all_tenants_data)
+    final_df = pd.merge(unique_df, all_tenants_df, left_on='TenantId', right_on='tenantId', how='left')
+    final_df.drop(columns=['tenantId'], inplace=True)
+
 # Print the filtered result
-print(unique_df)
-
-
-unique_df.to_csv('resources/CombinedStats_DEVPROD.csv', index=False)
-
-
-
+print(final_df)
+#final_df.to_csv('Output/CombinedStats_AUDIT_NA.csv', index=False)
--- ElasticCluster/config.py
+++ ElasticCluster/config.py
@@ -1,4 +1,12 @@
 # config.py
+#DEVPROD
 BASE_URL = 'https://vpc-ccgf-qa-devprod-cdgc-v2-es-v4za6cpj3x2r4gtmgtsv2a2o3u.us-west-2.es.amazonaws.com'
 # Uncomment the below line if you need the AUDIT cluster
 #BASE_URL = 'https://vpc-ccgf-qa-devprod-cdgc-audit-e-5hq67rm25gap6h53bmqnuejkaa.us-west-2.es.amazonaws.com'
+
+NA_API_BASE_URL = "https://cdgc-api.dm-us.informaticacloud.com/"
+DEVPROD_API_BASE_URL = "https://devprod-api.hawk.mrel.infaqa.com"
+
+# tenant_api_base_url definition moved here
+tenant_api_base_url = f"{NA_API_BASE_URL}/ccgf-tms/odata/v1/Tenants"
+
