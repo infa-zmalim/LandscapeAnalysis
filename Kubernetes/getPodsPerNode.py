95f16701e34247635ec13a61231fe64cc387438d
zameermalim <63711525+zameermalim@users.noreply.github.com>
2023-09-24 23:46:58 +0530

    V2 Added getPodsPerNode.py - changed the output to show CPU amd memory requests

--- Kubernetes/getPodsPerNode.py
+++ Kubernetes/getPodsPerNode.py
@@ -2,6 +2,15 @@ import subprocess
 import json
 from collections import defaultdict
 
+from Kubernetes.utils import parse_memory
+
+
+def parse_cpu(cpu):
+    if cpu.endswith('m'):
+        return float(cpu.rstrip('m')) / 1000
+    return float(cpu)
+
+
 def run_command(command):
     process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     output, error = process.communicate()
@@ -24,9 +33,20 @@ def get_pods_per_node():
         namespace = pod['metadata']['namespace']
 
         if node_name:  # Only append pods that are assigned to a node
+            cpu_requests = 0
+            memory_requests = 0
+            containers = pod['spec']['containers']
+            for container in containers:
+                resources = container.get('resources', {})
+                requests = resources.get('requests', {})
+                cpu_requests += parse_cpu(requests.get('cpu', '0'))
+                memory_requests += parse_memory(requests.get('memory', '0'))
+
             pods_per_node[node_name].append({
                 "pod_name": pod_name,
                 "namespace": namespace,
+                "cpu_requests": cpu_requests,
+                "memory_requests": memory_requests,
             })
 
     # Print the result
@@ -34,7 +54,7 @@ def get_pods_per_node():
         print(f"Node: {node}")
         print(f"Pod Count: {len(pods)}")
         for pod in pods:
-            print(f"  Namespace: {pod['namespace']}, PodName: {pod['pod_name']}")
+            print(f"  Namespace: {pod['namespace']}, PodName: {pod['pod_name']}, CPU Request: {pod['cpu_requests']:.2f} cores, Memory Request: {pod['memory_requests']} Mi")
         print()
 
 if __name__ == "__main__":
