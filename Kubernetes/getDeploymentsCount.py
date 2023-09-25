acd16ebf705ccd1a6ca2318328b981ba5327981d
zameermalim <63711525+zameermalim@users.noreply.github.com>
2023-09-25 23:15:56 +0530

    V2 Added prettyTable

--- Kubernetes/getDeploymentsCount.py
+++ Kubernetes/getDeploymentsCount.py
@@ -1,6 +1,7 @@
 import re
 from collections import Counter
 import yaml
+from prettytable import PrettyTable  # Importing PrettyTable
 
 from Kubernetes.utils import run_command
 
@@ -8,38 +9,31 @@ with open('resources/NON-PROD_clusters.yaml', 'r') as file:
     clusters = yaml.safe_load(file)
 
 def get_service_names(namespace):
-    # Fetch deployments from Kubernetes namespace
     cmd_output = run_command(f"kubectl get deployment -n {namespace}")
-
-    # Split the output by lines and skip the header
     lines = cmd_output.split('\n')[1:]
-
-    # Use regex to extract the service name
     service_names = [re.split('-\d+', line.split()[0])[0] for line in lines if line]
-
-    # Count occurrences of each service name
     counter = Counter(service_names)
-
-    # Sort by count
     sorted_services = sorted(counter.items(), key=lambda x: x[1], reverse=True)
-
     return sorted_services
 
 def main():
-
     for cluster in clusters:
-        # Update kubeconfig for the cluster
         run_command(cluster["config"])
-
-        # Fetch and process service names for each namespace in the cluster
         for namespace in cluster["namespaces"]:
             results = get_service_names(namespace)
-            # Determine the maximum length of the service names
-            max_length = max(len(service) for service, _ in results)
-            print(f"\n\n Results for namespace {namespace}:")
+
+            print(f"\n\nResults for Cluster: {cluster['config'].split('--name')[-1].strip()}, Namespace: {namespace}:")
+
+            # Creating a PrettyTable object and setting field names
+            table = PrettyTable()
+            table.field_names = ["Service", "Count"]
+
             for service, count in results:
-                # Use the string format method to align the output
-                print(f"{service},{' '*(max_length-len(service))}{count}")
+                # Adding rows to the PrettyTable object
+                table.add_row([service, count])
+
+            # Printing the PrettyTable
+            print(table)
 
 if __name__ == "__main__":
     main()
