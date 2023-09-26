bacbf291bf4f00e1ba75af62f49fa3d198700a78
zameermalim <63711525+zameermalim@users.noreply.github.com>
2023-09-26 15:13:57 +0530

    V1 Added CSV cgange

--- Kubernetes/getPodsPerNode.py
+++ Kubernetes/getPodsPerNode.py
@@ -1,15 +1,11 @@
+import csv
 import json
 import subprocess
 from collections import defaultdict
 
 import yaml
-from prettytable import PrettyTable
-from Kubernetes.utils import parse_memory, parse_cpu, run_command
 
-
-def color_text(text, color_code):
-    """Add ANSI color codes to text."""
-    return f"\033[{color_code}m{text}\033[0m"
+from Kubernetes.utils import run_command, parse_memory, parse_cpu
 
 
 def get_node_capacity():
@@ -20,109 +16,107 @@ def get_node_capacity():
     for node in nodes_json['items']:
         node_name = node['metadata']['name']
         cpu_capacity = parse_cpu(node['status']['capacity']['cpu'])
-        memory_capacity = parse_memory(node['status']['capacity']['memory']) * 1024
+        memory_capacity = parse_memory(node['status']['capacity']['memory'])
         node_capacity[node_name] = {"cpu": cpu_capacity, "memory": memory_capacity}
 
     return node_capacity
 
 
-def get_pods_per_node(cluster_name):
+def get_pods_per_node(cluster_name, csv_writer):
     node_capacity = get_node_capacity()
-
     pods_output = run_command("kubectl get pods --all-namespaces -o json")
     pods_json = json.loads(pods_output)
-
     pods_per_node = defaultdict(list)
 
     for pod in pods_json['items']:
         node_name = pod['spec'].get('nodeName')
+        if not node_name:
+            continue
+        if node_name not in node_capacity:
+            print(f"Warning: Node {node_name} not found in node_capacity")
+            continue
+
         pod_name = pod['metadata']['name']
         namespace = pod['metadata']['namespace']
+        cpu_requests = sum(
+            parse_cpu(container.get('resources', {}).get('requests', {}).get('cpu', '0')) for container in
+            pod['spec']['containers'])
+        memory_requests = sum(
+            parse_memory(container.get('resources', {}).get('requests', {}).get('memory', '0')) for container in
+            pod['spec']['containers'])
 
-        if node_name:
-            cpu_requests = 0
-            memory_requests = 0
-            containers = pod['spec']['containers']
-            for container in containers:
-                resources = container.get('resources', {})
-                requests = resources.get('requests', {})
-                cpu_requests += parse_cpu(requests.get('cpu', '0'))
-                memory_requests += parse_memory(requests.get('memory', '0'))
-
-            pods_per_node[node_name].append({
-                "pod_name": pod_name,
-                "namespace": namespace,
-                "cpu_requests": cpu_requests,
-                "memory_requests": memory_requests,
-            })
+        pods_per_node[node_name].append({
+            "pod_name": pod_name,
+            "namespace": namespace,
+            "cpu_requests": cpu_requests,
+            "memory_requests": memory_requests,
+        })
 
     for node, pods in pods_per_node.items():
-        table = PrettyTable()
-        table.field_names = ["Cluster", "Node", "Namespace", "Pod", "CPURequests (cores)", "MemoryRequests (Mi)",
-                             "CDGC CPU % Utilization", "CDGC Mem % Utilization",
-                             "Total CPU % Utilization", "Total Mem % Utilization"]
-        table.align = "l"
-
-        selected_cpu = 0
-        selected_memory = 0
-        total_cpu = 0
-        total_memory = 0
-
-        is_selected_namespace = any(
-            pod['namespace'].startswith("ccgf") or pod['namespace'].startswith("idmcp") or pod['namespace'].startswith("gpt") for pod in pods)
+        total_cpu = total_memory = selected_cpu = selected_memory = 0
 
         for pod in pods:
-            row = [
-                cluster_name if pod is pods[0] else '',
-                node if pod is pods[0] else '',
-                pod['namespace'],
-                pod['pod_name'],
-                f"{pod['cpu_requests']:.2f}",
-                f"{int(pod['memory_requests'])}",
-                '',
-                '',
-                '',
-                ''
-            ]
-
-            if pod['namespace'].startswith("ccgf") or pod['namespace'].startswith("idmcp") or pod['namespace'].startswith("gpt"):
-                row = [color_text(cell, '32') for cell in row]
+            if pod['namespace'].startswith(("ccgf", "idmcp", "gpt")):
                 selected_cpu += pod['cpu_requests']
                 selected_memory += pod['memory_requests']
-
             total_cpu += pod['cpu_requests']
             total_memory += pod['memory_requests']
 
-            table.add_row(row)
+            csv_writer.writerow({
+                "Cluster": cluster_name,
+                "Node": node,
+                "Namespace": pod['namespace'],
+                "Pod": pod['pod_name'],
+                "CPURequests (cores)": f"{pod['cpu_requests']:.2f}",
+                "MemoryRequests (Mi)": f"{int(pod['memory_requests'])}",
+            })
 
-        selected_cpu_utilization = f"{(selected_cpu / node_capacity[node]['cpu']) * 100:.2f}%"
-        selected_memory_utilization = f"{(selected_memory / node_capacity[node]['memory']) * 100:.2f}%"
-        total_cpu_utilization = f"{(total_cpu / node_capacity[node]['cpu']) * 100:.2f}%"
-        total_memory_utilization = f"{(total_memory / node_capacity[node]['memory']) * 100:.2f}%"
+        # Write total row to CSV for the current node
+        selected_cpu_utilization = (selected_cpu / node_capacity[node]['cpu']) * 100
+        selected_memory_utilization = (selected_memory / (1024 * node_capacity[node]['memory'])) * 100
+        total_cpu_utilization = (total_cpu / node_capacity[node]['cpu']) * 100
+        total_memory_utilization = (total_memory / (1024 * node_capacity[node]['memory'])) * 100
 
-        if is_selected_namespace:
-            selected_cpu_utilization = color_text(selected_cpu_utilization, '32')
-            selected_memory_utilization = color_text(selected_memory_utilization, '32')
-
-        table.add_row([cluster_name, node, "Total", len(pods), f"{total_cpu:.2f}", f"{int(total_memory)}",
-                       selected_cpu_utilization, selected_memory_utilization,
-                       total_cpu_utilization, total_memory_utilization])
-
-        print(table)
-        print()
+        csv_writer.writerow({
+            "Cluster": cluster_name,
+            "Node": node,
+            "Namespace": "Total",
+            "Pod": len(pods),
+            "CPURequests (cores)": f"{total_cpu:.2f}",
+            "MemoryRequests (Mi)": f"{int(total_memory)}",
+            "CDGC CPU % Utilization": f"{selected_cpu_utilization:.2f}%",
+            "CDGC Mem % Utilization": f"{selected_memory_utilization:.2f}%",
+            "Total CPU % Utilization": f"{total_cpu_utilization:.2f}%",
+            "Total Mem % Utilization": f"{total_memory_utilization:.2f}%",
+        })
 
 
 def get_pods_per_all_clusters():
     with open('resources/NON-PROD_clusters.yaml', 'r') as file:
         clusters_data = yaml.safe_load(file)
-    for cluster in clusters_data:
-        cluster_name = cluster.get('name', 'Unknown Cluster')
-        config_command = cluster.get('config')
-        if config_command:
-            subprocess.run(config_command, shell=True, check=True)
-            get_pods_per_node(cluster_name)
-        else:
-            print("Config command not found in cluster:", cluster)
+
+    fieldnames = [
+        "Cluster", "Node", "Namespace", "Pod",
+        "CPURequests (cores)", "MemoryRequests (Mi)",
+        "CDGC CPU % Utilization", "CDGC Mem % Utilization",
+        "Total CPU % Utilization", "Total Mem % Utilization"
+    ]
+
+    with open('Output/get_pods_per_all_clusters.csv', mode='w', newline='') as file:
+        csv_writer = csv.DictWriter(file, fieldnames=fieldnames)
+        csv_writer.writeheader()
+
+        for cluster in clusters_data:
+            cluster_name = cluster.get('name', 'Unknown Cluster')
+            config_command = cluster.get('config')
+            if config_command:
+                try:
+                    subprocess.run(config_command, shell=True, check=True)
+                    get_pods_per_node(cluster_name, csv_writer)
+                except subprocess.CalledProcessError as e:
+                    print(f"Error running config command for cluster {cluster_name}: {e}")
+            else:
+                print("Config command not found in cluster:", cluster)
 
 
 if __name__ == "__main__":
