dcda2b1f7493c84f2b6e94a2ff606b474914235d
zameermalim <63711525+zameermalim@users.noreply.github.com>
2023-09-27 23:40:41 +0530

    Added taints in getPodsPerNode.py

--- Kubernetes/getPodsPerNode.py
+++ Kubernetes/getPodsPerNode.py
@@ -12,22 +12,27 @@ def color_text(text, color_code):
     return f"\033[{color_code}m{text}\033[0m"
 
 
-def get_node_capacity():
+def get_node_capacity_and_taints():
     nodes_output = run_command("kubectl get nodes -o json")
     nodes_json = json.loads(nodes_output)
 
     node_capacity = {}
+    node_taints = {}
     for node in nodes_json['items']:
         node_name = node['metadata']['name']
         cpu_capacity = parse_cpu(node['status']['capacity']['cpu'])
         memory_capacity = parse_memory(node['status']['capacity']['memory']) * 1024
-        node_capacity[node_name] = {"cpu": cpu_capacity, "memory": memory_capacity}
+        taints = node['spec'].get('taints', [])
+        taints_str = ', '.join([f"{taint['key']}={taint.get('value', '')}:{taint['effect']}" for taint in taints])
 
-    return node_capacity
+        node_capacity[node_name] = {"cpu": cpu_capacity, "memory": memory_capacity}
+        node_taints[node_name] = taints_str
+
+    return node_capacity, node_taints
 
 
 def get_pods_per_node(cluster_name):
-    node_capacity = get_node_capacity()
+    node_capacity, node_taints = get_node_capacity_and_taints()
 
     pods_output = run_command("kubectl get pods --all-namespaces -o json")
     pods_json = json.loads(pods_output)
@@ -61,7 +66,8 @@ def get_pods_per_node(cluster_name):
             print(f"Warning: Node {node} not found in node_capacity")
             continue
         table = PrettyTable()
-        table.field_names = ["Cluster", "Node", "Namespace", "Pod", "CPURequests (cores)", "MemoryRequests (Mi)",
+        table.field_names = ["Cluster", "Node", "Taints", "Namespace", "Pod", "CPURequests (cores)",
+                             "MemoryRequests (Mi)",
                              "CDGC CPU % Utilization", "CDGC Mem % Utilization",
                              "Total CPU % Utilization", "Total Mem % Utilization"]
         table.align = "l"
@@ -79,6 +85,7 @@ def get_pods_per_node(cluster_name):
             row = [
                 cluster_name if pod is pods[0] else '',
                 node if pod is pods[0] else '',
+                node_taints[node] if pod is pods[0] else '',
                 pod['namespace'],
                 pod['pod_name'],
                 f"{pod['cpu_requests']:.2f}",
@@ -109,7 +116,7 @@ def get_pods_per_node(cluster_name):
             selected_cpu_utilization = color_text(selected_cpu_utilization, '32')
             selected_memory_utilization = color_text(selected_memory_utilization, '32')
 
-        table.add_row([cluster_name, node, "Total", len(pods), f"{total_cpu:.2f}", f"{int(total_memory)}",
+        table.add_row(["", "", "", "Total", len(pods), f"{total_cpu:.2f}", f"{int(total_memory)}",
                        selected_cpu_utilization, selected_memory_utilization,
                        total_cpu_utilization, total_memory_utilization])
 
@@ -122,11 +129,12 @@ def get_pods_per_all_clusters():
         cluster_name = cluster.get('name', 'Unknown Cluster')
         # if cluster_name == 'intcloud-ccgf-eks-devprod-usw2':
         config_command = cluster.get('config')
-        if config_command:
-            subprocess.run(config_command, shell=True, check=True)
-            get_pods_per_node(cluster_name)
-        else:
-            print("Config command not found in cluster:", cluster)
+    if config_command:
+        subprocess.run(config_command, shell=True, check=True)
+        get_pods_per_node(cluster_name)
+    else:
+        print("Config command not found in cluster:", cluster)
+
 
 if __name__ == "__main__":
     get_pods_per_all_clusters()
