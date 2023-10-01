9237b82045a2f72c0f5132996baf6b8fcdde6476
zameermalim <63711525+zameermalim@users.noreply.github.com>
2023-10-01 12:46:32 +0530

    Enhanced the script to give data from Telemetery as well

--- Kibana/KibanaServiceRequestCountPerTenant.py
+++ Kibana/KibanaServiceRequestCountPerTenant.py
@@ -1,88 +1,97 @@
 import configparser
 import json
 from urllib.parse import urljoin
-
 import pandas as pd
 import requests
-# Set display options for clearer output
-pd.set_option('display.max_columns', None)
-pd.set_option('display.width', None)
-pd.set_option('display.max_colwidth', None)
-pd.set_option('display.max_rows', 500)
 from ElasticCluster.config import tenant_api_base_url
+from ElasticCluster.utility_functions import modify_volume
 
-# Load the configuration file
-config = configparser.ConfigParser()
-config.read('config/config.ini')
+# Importing get_tenant_asset_data function
+from Kibana import getTenatAssetData
 
-# Read the configuration parameters
-url = config.get('QA', 'url')
-cert_file_path = config.get('QA', 'cert_file_path')
-key_file_path = config.get('QA', 'key_file_path')
-api_key = config.get('QA', 'apikey')
+def get_kibana_service_data():
+    # Load the configuration file
+    config = configparser.ConfigParser()
+    config.read('config/config.ini')
 
-# Fetch the time range from config.ini
-start_time = config.get('TimeRange', 'start_time')
-end_time = config.get('TimeRange', 'end_time')
-serviceName  = config.get('QueryParams','serviceName')
-objectName  = config.get('QueryParams','objectName')
+    # Set display options for clearer output
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.width', None)
+    pd.set_option('display.max_colwidth', None)
+    pd.set_option('display.max_rows', 500)
 
-# Read the payload from the JSON file for the second request
-with open('QA/Resources/DSLSearchRequestsPerTenant.json', 'r') as file:
-    payload = file.read()
-    # Replace the placeholders with actual values
-    payload = payload.replace("{{start_time}}", start_time).replace("{{end_time}}", end_time).replace("{{serviceName}}", serviceName).replace("{{objectName}}",objectName)
-    payload = json.loads(payload)
+    # Read the configuration parameters
+    url = config.get('QA', 'url')
+    cert_file_path = config.get('QA', 'cert_file_path')
+    key_file_path = config.get('QA', 'key_file_path')
+    api_key = config.get('QA', 'apikey')
+    start_time = config.get('TimeRange', 'start_time')
+    end_time = config.get('TimeRange', 'end_time')
+    serviceName  = config.get('QueryParams','serviceName')
+    objectName  = config.get('QueryParams','objectName')
 
-request_payload = json.dumps(payload)
+    # Read the payload from the JSON file for the second request
+    with open('QA/Resources/DSLSearchRequestsPerTenant.json', 'r') as file:
+        payload = file.read()
+        payload = payload.replace("{{start_time}}", start_time).replace("{{end_time}}", end_time).replace("{{serviceName}}", serviceName).replace("{{objectName}}", objectName)
+        payload = json.loads(payload)
 
-# Define the path
-path = "fluentbit-*-ccgf-*/_search"
+    request_payload = json.dumps(payload)
 
-# Join the base URL with the path
-full_url = urljoin(url, path)
-print("Full URL:", full_url)
+    # Define the path
+    path = "fluentbit-*-ccgf-*/_search"
 
-# Headers
-headers = {
-    "Content-Type": "application/json",
-    "x-api-key": api_key
-}
+    # Join the base URL with the path
+    full_url = urljoin(url, path)
 
-# Send the request
-try:
-    kibana_response = requests.post(full_url, headers=headers, data=request_payload, cert=(cert_file_path, key_file_path))
-    kibana_response.raise_for_status()
-except requests.exceptions.RequestException as e:
-    print("Error making the request:", e)
-    kibana_response = None
+    # Headers
+    headers = {
+        "Content-Type": "application/json",
+        "x-api-key": api_key
+    }
 
-# Fetch all tenants
-TMS_response = requests.get(tenant_api_base_url, headers={'accept': 'application/json'})
+    # Send the request
+    try:
+        kibana_response = requests.post(full_url, headers=headers, data=request_payload, cert=(cert_file_path, key_file_path))
+        kibana_response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print("Error making the request:", e)
+        kibana_response = None
 
-if TMS_response.status_code == 200:
+    # Fetch all tenants
+    TMS_response = requests.get(tenant_api_base_url, headers={'accept': 'application/json'})
+    if TMS_response.status_code != 200:
+        print("Error fetching tenants:", TMS_response.status_code)
+        print(TMS_response.text)
+
     all_tenants_data_TMS = TMS_response.json().get('value', [])
     all_tenants_TMS_df = pd.DataFrame(all_tenants_data_TMS)
-else:
-    print("Error fetching tenants:", TMS_response.status_code)
-    print(TMS_response.text)
 
-if kibana_response and kibana_response.status_code == 200:
-    request_payload = kibana_response.json()
-    bucket_data = request_payload['aggregations']['0']['buckets']
-    table_data = []
+    merged_df = None
+    if kibana_response and kibana_response.status_code == 200:
+        request_payload = kibana_response.json()
+        bucket_data = request_payload['aggregations']['0']['buckets']
+        table_data = []
 
-    for item in bucket_data:
-        org_id = item['key']
-        doc_count = item['doc_count']
-        count_200 = item['2-bucket']['2-metric']['value']
-        count_500 = item['3-bucket']['3-metric']['value']
-        table_data.append([org_id, doc_count, count_200, count_500])
+        for item in bucket_data:
+            org_id = item['key']
+            doc_count = item['doc_count']
+            count_200 = item['2-bucket']['2-metric']['value']
+            count_500 = item['3-bucket']['3-metric']['value']
+            table_data.append([org_id, doc_count, count_200, count_500])
 
-    df = pd.DataFrame(table_data, columns=['orgId', 'Doc Count', 'Count of 200s', 'Count of 500s'])
-    merged_df = pd.merge(df, all_tenants_TMS_df[['tenantId', 'name']], left_on='orgId', right_on='tenantId', how='left').drop('tenantId', axis=1)
+        df = pd.DataFrame(table_data, columns=['orgId', f'{serviceName} {objectName} requests', 'Count of 200s', 'Count of 500s'])
+        cols_to_modify = [f'{serviceName} {objectName} requests', 'Count of 200s', 'Count of 500s']
+        for col in cols_to_modify:
+            df[col] = df[col].apply(modify_volume)
+        merged_df = pd.merge(df, all_tenants_TMS_df[['tenantId', 'name']], left_on='orgId', right_on='tenantId', how='left').drop('tenantId', axis=1)
 
-    print(merged_df)
-else:
-    print("Error:", kibana_response.status_code if kibana_response else "No response")
-    print(kibana_response.text if kibana_response else "")
+    tenant_asset_df = getTenatAssetData.get_tenant_asset_data()
+    final_df = pd.merge(merged_df, tenant_asset_df, left_on='orgId', right_on='Org', how='left')
+
+    return final_df
+
+# For debugging purposes
+if __name__ == "__main__":
+    df = get_kibana_service_data()
+    print(df)
