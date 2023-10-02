a49202a67838664b603a3f9b971da45769d31783
zameermalim <63711525+zameermalim@users.noreply.github.com>
2023-10-03 00:58:37 +0530

    Created new script to get Indices Performance stats

---
+++ ElasticCluster/getIndicesPerformanceStats.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import requests
+from Kibana.getTelemetryData import getTelemetrydata
+from Kibana import getTenantAssetData
+from Kibana.getTenantAssetData import get_tenant_asset_data
+from utility_functions import convert_size_to_mb, modify_volume, extract_tenant_id, extract_org_id
+import os
+from config import BASE_URL
+
+# Set display options
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', None)
+pd.set_option('display.max_rows',50)
+
+def getIndicesPerformanceStats():
+    # Fetching data from the API for indices stats
+    indices_response = requests.get(f'{BASE_URL}/_cat/indices?h=index,docs.count,store.size,refresh.time,search.query_total,search.query_time,search.query_current&format=json')
+    indices_response.raise_for_status()  # Check if request was successful
+    indices_data = indices_response.json()
+    indices_df = pd.DataFrame(indices_data)
+    # print(f"Indices:", indices_df)
+    # print(f"Indices:\n{indices_df.to_string()}")
+    indices_df['index'] = indices_df['index'].apply(extract_tenant_id)
+    telemetry_df = get_tenant_asset_data()
+    merged_df = pd.merge(indices_df, telemetry_df, left_on='index', right_on='Org', how='left')
+    merged_df['search.query_total'] = pd.to_numeric(merged_df['search.query_total'])
+    merged_df = merged_df.sort_values(by='search.query_total', ascending=False)
+    merged_df['search.query_total'] = merged_df['search.query_total'].apply(modify_volume)
+
+    return merged_df
+
+if __name__ == "__main__":
+    getIndicesPerformanceStats=getIndicesPerformanceStats()
+    print(getIndicesPerformanceStats)
