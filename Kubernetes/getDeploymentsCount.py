962fb31cc7dd4eba3ff3fb9692bfb1d3cc90a962
zameermalim <63711525+zameermalim@users.noreply.github.com>
2023-10-04 18:57:31 +0530

    Made getDeploymentsCount.py a function

--- Kubernetes/getDeploymentsCount.py
+++ Kubernetes/getDeploymentsCount.py
@@ -1,23 +1,20 @@
 from prettytable import PrettyTable
 from Kubernetes.utils.utils import run_command, clusters, get_service_names
 
-
-def getDeploymentsCount():
+def printDeploymentsCount():
     for cluster in clusters:
         run_command(cluster["config"])
         for namespace in cluster["namespaces"]:
             results = get_service_names(namespace)
 
-            # print(f"\n\nResults for Cluster: {cluster['config'].split('--name')[-1].strip()}:")
+            print(f"\n\nResults for Cluster: {cluster['config'].split('--name')[-1].strip()}:")
 
             table = PrettyTable()
-            table.field_names = ["Cluster","Namespace", "Service", "Count"]
-
+            table.field_names = ["Cluster", "Namespace", "Service", "Count"]
             for service, count in results:
-                table.add_row([cluster['config'].split('--name')[-1].strip(),namespace, service, count])
+                table.add_row([cluster['config'].split('--name')[-1].strip(), namespace, service, count])
 
-            return table
+            print(table)
 
 if __name__ == "__main__":
-    getDeploymentsCount = getDeploymentsCount()
-    print(getDeploymentsCount)
+    printDeploymentsCount()
