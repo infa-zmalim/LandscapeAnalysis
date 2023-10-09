7bdec0bdfe1888382c30fc69d706927bb6487ea6
zameermalim <63711525+zameermalim@users.noreply.github.com>
2023-10-09 13:17:43 +0530

    Made changes to getHPAStats.pyto return the output in a DF

--- Kubernetes/getHPAStats.py
+++ Kubernetes/getHPAStats.py
@@ -1,27 +1,46 @@
 from Kubernetes.utils.utils import run_command, clusters
-
+import pandas as pd
+# Set display options for clearer output
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', None)
+pd.set_option('display.max_rows', None)
+import re
 
 def get_hpa(namespace):
-    # Fetch HPAs from Kubernetes namespace
     cmd_output = run_command(f"kubectl get hpa -n {namespace}")
-
-    # Check if cmd_output is not empty and split the output by lines
     lines = cmd_output.split('\n') if cmd_output else []
 
-    # Skip the header and print the HPA details
+    if not lines:
+        return []
+
+    headers = re.split(r'\s{2,}', lines[0])  # Split headers on 2 or more spaces
+
+    hpa_data = []
     for line in lines[1:]:
         if line:
-            print(line)
+            values = re.split(r'\s{2,}', line)
+            hpa_dict = dict(zip(headers, values))
+            hpa_data.append(hpa_dict)
 
-def main():
-    for cluster in clusters:
-        # Update kubeconfig for the cluster
-        run_command(cluster["config"])
+    return hpa_data
 
-        # Fetch and display HPA details for each namespace in the cluster
-        for namespace in cluster["namespaces"]:
-            print(f"\n\nHPA details for namespace {namespace}:")
-            get_hpa(namespace)
+def getHPAStatsForCluster(cluster):
+    # Update kubeconfig for the cluster
+    run_command(cluster["config"])
+
+    cluster_hpa_data = []
+    # Fetch HPA details for each namespace in the cluster
+    for namespace in cluster["namespaces"]:
+        hpa_data = get_hpa(namespace)
+        cluster_hpa_data.extend(hpa_data)
+
+    # Convert list of dictionaries to a DataFrame for this cluster
+    df = pd.DataFrame(cluster_hpa_data)
+    return df.sort_values(by='REPLICAS', ascending=False)
 
 if __name__ == "__main__":
-    main()
+    for cluster in clusters:
+        print(f"\n\nHPA details for cluster {cluster['name']}:\n")  # Assuming cluster has a 'name' key
+        df_hpa_stats = getHPAStatsForCluster(cluster)
+        print(df_hpa_stats)
