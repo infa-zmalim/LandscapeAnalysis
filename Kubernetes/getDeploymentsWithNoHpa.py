a151ed057928a1ca8d9c8ba38b288e23b01d8a7b
zameermalim <63711525+zameermalim@users.noreply.github.com>
2023-10-09 13:34:00 +0530

    Made changes in the getDeploymentsWithNoHpa.py to use utils.py

--- Kubernetes/getDeploymentsWithNoHpa.py
+++ Kubernetes/getDeploymentsWithNoHpa.py
@@ -1,26 +1,27 @@
 import subprocess
 import json
 
-from Kubernetes.utils.utils import clusters, run_command
+from Kubernetes.utils.utils import clusters, run_command, get_deployments, get_hpas
+
 
 def getDeploymentsWithNoHpa(NameSpace):
-    # Equivalent to kubectl get deployments command
-    cmd_output = subprocess.getoutput(f"kubectl get deployments -n {NameSpace} -o json")
-    deployments = json.loads(cmd_output)
+    # Get all deployments in the namespace
+    deployments = get_deployments(NameSpace)
+
+    # Get all HPAs in the namespace
+    hpas = get_hpas(NameSpace)
+    hpa_names = {hpa["metadata"]["name"] for hpa in hpas["items"]}
 
     print(f"\nDeployments with no HPA in namespace {NameSpace}:\n")
     for deployment in deployments["items"]:
         deploymentName = deployment["metadata"]["name"]
-        if NameSpace not in deploymentName:
-            hpaExists = subprocess.getoutput(f"kubectl get hpa {deploymentName}-hpa -n {NameSpace} -o name --ignore-not-found")
-            if not hpaExists:
-                print(deploymentName)
+        if NameSpace not in deploymentName and f"{deploymentName}-hpa" not in hpa_names:
+            print(deploymentName)
 
 if __name__ == "__main__":
     for cluster in clusters:
         # Set kubeconfig for the cluster
         run_command(cluster["config"])
-
         print(f"\n\nDetails for cluster {cluster['name']}:\n")  # Assuming cluster has a 'name' key
 
         # Loop through each namespace in the cluster
