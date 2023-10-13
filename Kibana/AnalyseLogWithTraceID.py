255d78a3f1888e784f84ad77bd504cc31ebcf389
zameermalim <63711525+zameermalim@users.noreply.github.com>
2023-10-13 13:40:27 +0530

    Made changes in the AnalyseLogWithTraceID.py to use the datetime inside the message body not the fluentbit timestamp

--- Kibana/AnalyseLogWithTraceID.py
+++ Kibana/AnalyseLogWithTraceID.py
@@ -1,41 +1,42 @@
 import configparser
 import json
 from urllib.parse import urljoin
+
 import pandas as pd
 import requests
 
+from Kibana.utils.utils import load_config
+
+
 def get_log_details(traceId):
+    configurations = load_config()  # Using the new load_config function
+
     # Set display options for clearer output
     pd.set_option('display.max_columns', None)
     pd.set_option('display.width', None)
     pd.set_option('display.max_colwidth', None)
     pd.set_option('display.max_rows', None)
 
-    # Load the configuration file
-    config = configparser.ConfigParser()
-    config.read('config/config.ini')
-
-    # Read the configuration parameters
-    url = config.get('QA', 'url')
-    cert_file_path = config.get('QA', 'cert_file_path')
-    key_file_path = config.get('QA', 'key_file_path')
-    api_key = config.get('QA', 'apikey')
-    start_time = config.get('TimeRange', 'start_time')
-    end_time = config.get('TimeRange', 'end_time')
-    serviceName = config.get('QueryParams', 'serviceName')
-    # traceId = config.get('QueryParams', 'traceId')
+    # Load configurations
+    url = configurations['url']
+    cert_file_path = configurations['cert_file_path']
+    key_file_path = configurations['key_file_path']
+    api_key = configurations['apikey']
+    start_time = configurations.get('start_time', '')
+    end_time = configurations.get('end_time', '')
+    serviceName = configurations.get('servicename', '')
+    objectName = configurations.get('objectname', '')
+    path = configurations.get('path', '')
 
     # Read the payload from the JSON file for the second request
-    with open('QA/Resources/DSLLogWithTraceID.json', 'r') as file:
+    with open('Resources/DSLLogWithTraceID.json', 'r') as file:
         payload = file.read()
-        payload = payload.replace("{{start_time}}", start_time).replace("{{end_time}}", end_time).replace("{{serviceName}}", serviceName).replace("{{traceId}}", traceId)
+        payload = payload.replace("{{start_time}}", start_time).replace("{{end_time}}", end_time).replace(
+            "{{serviceName}}", serviceName).replace("{{traceId}}", traceId)\
+            # .replace("{{objectName}}", objectName)
         payload = json.loads(payload)
 
     request_payload = json.dumps(payload)
-
-    # Define the path
-    path = "fluentbit-*-ccgf-*/_search"
-
     # Join the base URL with the path
     full_url = urljoin(url, path)
 
@@ -47,7 +48,8 @@ def get_log_details(traceId):
 
     # Send the request
     try:
-        kibana_response = requests.post(full_url, headers=headers, data=request_payload, cert=(cert_file_path, key_file_path))
+        kibana_response = requests.post(full_url, headers=headers, data=request_payload,
+                                        cert=(cert_file_path, key_file_path))
         kibana_response.raise_for_status()
     except requests.exceptions.RequestException as e:
         print("Error making the request:", e)
@@ -67,14 +69,14 @@ def get_log_details(traceId):
             # Each record will be a dictionary
             record = {}
 
-            # Extract the timestamp
-            record['@timestamp'] = hit.get('fields', {}).get('@timestamp', [None])[0]
+            # Extract the datetime from message part
+            message_content = json.loads(hit.get('fields', {}).get('message', [None])[0])
 
+            record['datetime'] = message_content.get('datetime')
 
             # Extract the parsed.message
             fields = hit.get('fields', {})
             record['parsed.message'] = fields.get('parsed.message', [None])[0]
-
 
             # Append the record to the extracted_data list
             extracted_data.append(record)
@@ -82,16 +84,17 @@ def get_log_details(traceId):
         # Convert the list of dictionaries into a DataFrame for better visualization and further analysis
         df = pd.DataFrame(extracted_data)
 
-        # Convert the @timestamp column to datetime type
-        df['@timestamp'] = pd.to_datetime(df['@timestamp'])
+        # Convert the datetime column to datetime type
+        df['datetime'] = pd.to_datetime(df['datetime'])
 
-        # Calculate the time difference between each timestamp
-        df['time_difference'] = df['@timestamp'].diff().dt.total_seconds().fillna(0)
+        # Calculate the time difference between each datetime
+        df['time_difference'] = df['datetime'].diff().dt.total_seconds().fillna(0)
 
-        return (df)
+        return df
 
     else:
         print("No response or response has an error.")
+
 
 if __name__ == "__main__":
     # Load the configuration file
